41d80979c7aa59fc7e5ca29a05932106bcc0fffc
kakelay <kakelay18052002@gmail.com>
2023-08-01 09:29:51 +0700

    lastcommit error request

--- lib/register.dart
+++ lib/register.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: library_private_types_in_public_api, unnecessary_brace_in_string_interps, unnecessary_string_interpolations, avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field_with_validator/intl_phone_field.dart';
-
-import 'otp.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -11,6 +12,15 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  TextEditingController countrycode = TextEditingController();
+
+  var phone = '';
+  @override
+  void initState() {
+   // countrycode.text = "+855";
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +28,7 @@ class _RegisterState extends State<Register> {
         elevation: 0,
         backgroundColor: const Color(0xfff7f6fb),
         leading: GestureDetector(
-            onTap: () => Navigator.pop(context),
+          onTap: () => Navigator.pop(context),
           child: const Icon(
             Icons.arrow_back_ios_new,
             size: 32,
@@ -30,174 +40,160 @@ class _RegisterState extends State<Register> {
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
-            child: Column(
-              children: [
-               
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple.shade50,
-                      shape: BoxShape.circle,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade50,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset('assets/images/phone.png')),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  const Text(
+                    'Registration',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                     ),
-                    child: Image.asset('assets/images/phone.png')),
-                const SizedBox(
-                  height: 24,
-                ),
-                const Text(
-                  'Registration',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "Add your phone number. we'll send you a verification code so we know you're real",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black38,
+                  const SizedBox(
+                    height: 10,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 28,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(28),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
+                  const Text(
+                    "Add your phone number. we'll send you a verification code so we know you're real",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black38,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  child: Column(
-                    children: [
-                      /// newpick  country
-                      IntlPhoneField(
-                        decoration: InputDecoration(
-                          labelText: 'Phone Number',
-                          labelStyle: TextStyle(color: Colors.purple.shade900),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: const BorderSide(),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.purple.shade500),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.purple.shade500),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          suffixIcon: const Icon(
-                            Icons.check_circle,
-                            color: Colors.green,
-                            size: 22,
-                          ),
-                        ),
-                        initialCountryCode: 'KH',
-                        showCountryFlag: true,
-                        invalidNumberMessage: AutofillHints.telephoneNumber,
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                        autofocus: true,
-                        dropdownIcon: Icon(
-                          Icons.arrow_drop_down_sharp,
-                          color: Colors.purple.shade500,
-                        ),
-                        onChanged: (phone) {
-                          print(phone.completeNumber);
-                        },
+                  const SizedBox(
+                    height: 28,
+                  ),
+                  SingleChildScrollView(
+                    child: Container(
+                      padding: const EdgeInsets.all(28),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      const SizedBox(
-                        height: 22,
-                      ),
-////////////////////////////////////////////////////////////////////////////////
-                      // TextFormField(
-
-                      //   keyboardType: TextInputType.number,
-                      //   style: const TextStyle(
-                      //     fontSize: 18,
-                      //     fontWeight: FontWeight.bold,
-                      //   ),
-                      //   decoration: InputDecoration(
-                      //     enabledBorder: OutlineInputBorder(
-                      //       borderSide: const BorderSide(color: Colors.black12),
-                      //       borderRadius: BorderRadius.circular(10),
-                      //     ),
-                      //     focusedBorder: OutlineInputBorder(
-                      //       borderSide: const BorderSide(color: Colors.black12),
-                      //       borderRadius: BorderRadius.circular(10),
-                      //     ),
-                      //     prefix: const Row(
-                      //       // Updated: Use Row widget to display both country code and padding.
-                      //       mainAxisSize: MainAxisSize.min,
-                      //       children: [
-                      //         Padding(
-                      //           padding: EdgeInsets.symmetric(horizontal: 8),
-                      //           child: Text(
-                      //             '(+885)',
-                      //             style: TextStyle(
-                      //               fontSize: 18,
-                      //               fontWeight: FontWeight.bold,
-                      //             ),
-                      //           ),
-                      //         ),
-                      //         VerticalDivider(), // Optional: Add a vertical divider between the country code and the input field.
-                      //       ],
-                      //     ),
-                      //     suffixIcon: const Icon(
-                      //       Icons.check_circle,
-                      //       color: Colors.green,
-                      //       size: 32,
-                      //     ),
-                      //   ),
-                      // ),
-
-                      const SizedBox(
-                        height: 22,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => const Otp()),
-                            );
-                          },
-                          style: ButtonStyle(
-                            foregroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white),
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.purple),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            /// newpick  country
+                            IntlPhoneField(
+                              decoration: InputDecoration(
+                                //labelText: 'Phone Number',
+                                hintText: 'Number Phone',
+                                labelStyle:
+                                    TextStyle(color: Colors.purple.shade900),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: const BorderSide(),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.purple.shade500),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.purple.shade500),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                suffixIcon: const Icon(
+                                  Icons.check_circle,
+                                  color: Colors.green,
+                                  size: 22,
+                                ),
                               ),
+                              initialCountryCode: 'KH',
+                              showCountryFlag: true,
+                              invalidNumberMessage:
+                                  AutofillHints.telephoneNumber,
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                              autofocus: true,
+                              dropdownIcon: Icon(
+                                Icons.arrow_drop_down_sharp,
+                                color: Colors.purple.shade500,
+                              ),
+                              keyboardType: TextInputType.phone,
+                              onChanged: (value) {
+                                print(
+                                    "Hi----------------------------------------------------------");
+                                print(value.completeNumber);
+
+                                phone = value.completeNumber;
+                              },
                             ),
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.all(14.0),
-                            child: Text(
-                              'Send',
-                              style: TextStyle(fontSize: 16),
+
+                            const SizedBox(
+                              height: 22,
                             ),
-                          ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () async {
+                                  print(countrycode.text + phone);
+                                  await FirebaseAuth.instance.verifyPhoneNumber(
+                                    phoneNumber: '${countrycode.text + phone}',
+                                    verificationCompleted:
+                                        (PhoneAuthCredential credential) {},
+                                    verificationFailed:
+                                        (FirebaseAuthException e) {
+                                      print(e.code);
+                                    },
+                                    codeSent: (String verificationId,
+                                        int? resendToken) {},
+                                    codeAutoRetrievalTimeout:
+                                        (String verificationId) {},
+                                  );
+                                  // Navigator.of(context).push(   //https://firebase.google.com/docs/auth/flutter/manage-users
+                                  //   MaterialPageRoute(
+                                  //       builder: (context) => const Otp()),
+                                  // );
+                                },
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.purple),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(24.0),
+                                    ),
+                                  ),
+                                ),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(14.0),
+                                  child: Text(
+                                    'Send the code',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
